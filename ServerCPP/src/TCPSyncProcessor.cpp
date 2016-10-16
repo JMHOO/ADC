619da0a899a074977baf50b9029f9665f1311644
Jiaming <hujiaming@hotmail.com>
2016-10-16 00:48:02 -0700

    fix tcpserver bug

--- ServerCPP/src/TCPSyncProcessor.cpp
+++ ServerCPP/src/TCPSyncProcessor.cpp
@@ -25,59 +25,64 @@ bool TCPSyncServerProcessor::Execute( void * pdata )
     
     tcpHelper.SetSocketProperty(pConnParam->socketid);
     
-    // Receive
-    recvedlen = tcpHelper.RecvInfo( pConnParam->socketid, sBuffer, (int)(sizeof(ADCS::PACK_HEADER)) );
-    
-    if( (int)(sizeof(ADCS::PACK_HEADER)) > recvedlen )
+    while(true)
     {
-        if( recvedlen == 0 )
+        memset(sBuffer, 0, 1024);
+        
+        // Receive
+        recvedlen = tcpHelper.RecvInfo( pConnParam->socketid, sBuffer, (int)(sizeof(ADCS::PACK_HEADER)) );
+        
+        if( (int)(sizeof(ADCS::PACK_HEADER)) > recvedlen )
         {
-            if(m_logger)m_logger->Info("TCPSyncServerProcessor::Execute, receive nothing, client might closed.");
+            if( recvedlen == 0 )
+            {
+                if(m_logger)m_logger->Info("TCPSyncServerProcessor::Execute, receive nothing, client might close.");
+            }
+            else
+            {
+                if(m_logger)m_logger->Error("TCPSyncServerProcessor::Execute, receive package header error. Expect to receive %d bytes, in fact, it receive %d bytes", sizeof(ADCS::PACK_HEADER), recvedlen);
+            }
+            tcpHelper.Close(pConnParam->socketid);
+            delete pConnParam;
+            return true;
+        }
+        
+        int nRestDataLen = ntohl(ptrHeader->Length) - recvedlen;
+        len = tcpHelper.RecvInfo( pConnParam->socketid, sBuffer + recvedlen, nRestDataLen );
+        if( len != nRestDataLen )
+        {
+            if(m_logger)m_logger->Error("TCPSyncServerProcessor::Execute, receive package payload error. Expect to receive %d bytes, in fact, it receive %d bytes", nRestDataLen, len);
+            tcpHelper.Close(pConnParam->socketid);
+            delete pConnParam;
+            return true;
+        }
+        
+        
+        // we got the whold package, deal with it
+        IPacket* packet = IPacket::CreatePackage(sBuffer, ntohl(ptrHeader->Length), pConnParam->socketid);
+        if( packet )
+        {
+            packet->Process();
+            const char* ptrResponse = nullptr;
+            unsigned long ulResponseLength = 0;
+            packet->ToBytes(ptrResponse, ulResponseLength);
+            
+            // send whole package
+            len = tcpHelper.SendInfo( pConnParam->socketid, ptrResponse, (int)ulResponseLength );
+            if( (int)ulResponseLength != len )
+            {
+                if(m_logger)m_logger->Error("TCPSyncServerProcessor::Execute, sending package header error. Expect to send %d bytes, in fact, it sent %d bytes", ulResponseLength, len);
+                tcpHelper.Close(pConnParam->socketid);
+                delete pConnParam;
+                return true;
+            }
+            
+            delete packet;
         }
         else
         {
-            if(m_logger)m_logger->Error("TCPSyncServerProcessor::Execute, receive package header error. Expect to receive %d bytes, in fact, it receive %d bytes", sizeof(ADCS::PACK_HEADER), recvedlen);
+            if(m_logger)m_logger->Error("TCPSyncServer Invalid packet, dropped it. Content: %s", sBuffer+sizeof(ADCS::PACK_HEADER));
         }
-        tcpHelper.Close(pConnParam->socketid);
-        delete pConnParam;
-        return true;
-    }
-    
-    int nRestDataLen = ntohl(ptrHeader->Length) - recvedlen;
-    len = tcpHelper.RecvInfo( pConnParam->socketid, sBuffer + recvedlen, nRestDataLen );
-    if( len != nRestDataLen )
-    {
-        if(m_logger)m_logger->Error("TCPSyncServerProcessor::Execute, receive package payload error. Expect to receive %d bytes, in fact, it receive %d bytes", nRestDataLen, len);
-        tcpHelper.Close(pConnParam->socketid);
-        delete pConnParam;
-        return true;
-    }
-    
-    
-    // we got the whold package, deal with it
-    IPacket* packet = IPacket::CreatePackage(sBuffer, ntohl(ptrHeader->Length), pConnParam->socketid);
-    if( packet )
-    {
-        packet->Process();
-        const char* ptrResponse = nullptr;
-        unsigned long ulResponseLength = 0;
-        packet->ToBytes(ptrResponse, ulResponseLength);
-        
-        // send whole package
-        len = tcpHelper.SendInfo( pConnParam->socketid, ptrResponse, (int)ulResponseLength );
-        if( (int)ulResponseLength != len )
-        {
-            if(m_logger)m_logger->Error("TCPSyncServerProcessor::Execute, sending package header error. Expect to send %d bytes, in fact, it sent %d bytes", ulResponseLength, len);
-            tcpHelper.Close(pConnParam->socketid);
-            delete pConnParam;
-            //return true;
-        }
-        
-        delete packet;
-    }
-    else
-    {
-        if(m_logger)m_logger->Error("TCPSyncServer Invalid packet, dropped it. Content: %s", sBuffer+sizeof(ADCS::PACK_HEADER));
     }
     
     /* test code, just echo response
