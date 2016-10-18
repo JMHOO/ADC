b13d29ddb195406577a076111e4847a0280376f1
Jiaming <hujiaming@hotmail.com>
2016-10-18 11:55:17 -0700

    add logs for RPCServer

--- ServerCPP/src/CKvJSONRPCService.h
+++ ServerCPP/src/CKvJSONRPCService.h
@@ -2,15 +2,19 @@
 #define __CKvJSONRPCService_H_
 
 #include "CKvRPCServiceInterface.h"
+#include "GLog.h"
 
 class CKvJSONRPCService : public ICKvRPCService
 {
 public:
-    CKvJSONRPCService(jsonrpc::AbstractServerConnector &connector);
+    CKvJSONRPCService(jsonrpc::AbstractServerConnector &connector, ILog* plogger);
     
     virtual Json::Value Put(const std::string& param1, const std::string& param2);
     virtual Json::Value Get(const std::string& param1);
     virtual Json::Value Delete(const std::string& param1);
+    
+private:
+    ILog* logger;
 };
 
 #endif
