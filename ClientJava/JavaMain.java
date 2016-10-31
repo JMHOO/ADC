ea23261cdd89fc263ccdddbc96c71f6627cdfabf
Yuyang105 <yuyang.yu105@gmail.com>
2016-10-31 16:04:48 -0700

    project-2

--- ClientJava/JavaMain.java
+++ ClientJava/JavaMain.java
@@ -1,21 +1,29 @@
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Vector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-  
+
+
+
 public class JavaMain {
 	
 	private static String serverDomainName = "uw.umx.io";
 	private static String serverIP ="73.140.72.152";
 	private static String serverPortNumber = "15001";
+	private static Vector<String> hostList = new Vector();
 
 	private static String inputFile = "src/kvp-operations.csv";
 	private static final String outputFileTcp = "src/output_tcp.csv";
@@ -48,70 +56,119 @@ public class JavaMain {
 				System.out.println("Please enter the server port number: ");
 				serverPortNumber = input.nextLine();
     		}
-//			JavaClient client_tcp = new JavaClient(serverIP, Integer.parseInt(serverPortNumber));
-//       	client_tcp.runOperations(inputFile, outputFileTcp, "TCP");
-        	
-			// Create socket
-			Socket socket = new Socket(serverIP, Integer.parseInt(serverPortNumber));
-			// Create input stream to receive data
-			fromServer = new DataInputStream(socket.getInputStream());
-			// Create output stream to send data
-			toServer = new DataOutputStream(socket.getOutputStream());
-
 			
-        	initialOutput(outputFileTcp);
-        	List<String[]> operations = csv.readCVS(inputFile);
-        	for (int i = 1; i <operations.size(); i++) {
-        		String operate = operations.get(i)[0];
-        		String key = operations.get(i)[1];
-        		String value = "";
-        		if (operate.equals("PUT"))
-        			value = operations.get(i)[2];
-        		
-//        		// Create socket
-//    			Socket socket = new Socket(serverIP, Integer.parseInt(serverPortNumber));
-//    			// Create input stream to receive data
-//    			fromServer = new DataInputStream(socket.getInputStream());
-//    			// Create output stream to send data
-//    			toServer = new DataOutputStream(socket.getOutputStream());
-        		
-        		// execute TCP
-        		String json = createJsonPayload(operate, key, value);
-        		Long requestTime = System.currentTimeMillis();
-        		//System.out.println(json);
-        		
-        		// protocol
-        		toServer.writeInt(0);
-        		toServer.writeInt(0);
-        		toServer.writeInt(16 + json.length());
-        		toServer.writeInt(0);
-        		toServer.writeBytes(json);
-        		toServer.flush();
-        		
-        		byte[] header = new byte[16];
-        		fromServer.read(header, 0, 16);
-        		
-        		// where magic happens
-        		int len = (int) ((((header[8] & 0xff) << 24) | ((header[9] & 0xff) << 16) | ((header[10] & 0xff) << 8) | ((header[11] & 0xff) << 0)));    
-        		byte[] message = new byte[len];
-        		fromServer.read(message, 0, len);
-        		System.out.println(new String(message));
-
-        		Long responseTime = System.currentTimeMillis();
-        		
-        		String duration = String.valueOf(responseTime - requestTime);
-        		String[] record = {operate, key, value, String.valueOf(message), duration};
-        		csv.appendCSV(outputFileTcp, record);
-        	}
-        	toServer.close();
-        	fromServer.close();
-        	socket.close();
+			//service discovery
+			serviceDiscover();
+			
+			// execute operations
+			//executeOperation();
+			
+			
     	}
 		catch(Exception ex) {
 			ex.printStackTrace();
 		}
 		input.close();
     }
+    
+    public static void serviceDiscover() throws NumberFormatException, UnknownHostException, IOException {
+    	// Create socket
+    	Socket socket = new Socket(serverIP, Integer.parseInt(serverPortNumber));
+    	// Create input stream to receive data
+    	fromServer = new DataInputStream(socket.getInputStream());
+    	// Create output stream to send data
+    	toServer = new DataOutputStream(socket.getOutputStream());
+    	
+    	// build json string to get server list
+    	String jsonRequest = "{\"jsonagent\":\"1.0\",\"operate\":\"getserverlist\"}";
+    	
+    	// protocol
+		toServer.writeInt(0);
+		toServer.writeInt(0);
+		toServer.writeInt(16 + jsonRequest.length());
+		toServer.writeInt(0);
+		toServer.writeBytes(jsonRequest);
+		toServer.flush();
+		
+		// read response
+		byte[] header = new byte[16];
+		fromServer.read(header, 0, 16);
+		
+		// where magic happens
+		int len = (int) ((((header[8] & 0xff) << 24) | ((header[9] & 0xff) << 16) | ((header[10] & 0xff) << 8) | ((header[11] & 0xff) << 0)));    
+		byte[] message = new byte[len];
+		fromServer.read(message, 0, len);
+		System.out.println(new String(message));
+		
+		// phrase json
+		String[] a = new String(message).split("[\\[\\]]");
+		System.out.println(a[1]);
+		String[] b = a[1].split("address\":\"");
+		for (int i = 1; i < b.length; i++) {
+			String[] c = b[i].split("\",\"");
+			String[] d = c[1].split("port\"");
+			String[] e = d[1].split("}");
+			StringBuffer sb = new StringBuffer();
+			sb.append(c[0]).append(e[0]);
+			// add to the host list
+			hostList.add(sb.toString());		// format: ip_address:port_number
+		}
+    }
+    
+    
+    // Execute operations
+    public static void executeOperation() throws NumberFormatException, UnknownHostException, IOException {
+		// Create socket
+		Socket socket = new Socket(serverIP, Integer.parseInt(serverPortNumber));
+		// Create input stream to receive data
+		fromServer = new DataInputStream(socket.getInputStream());
+		// Create output stream to send data
+		toServer = new DataOutputStream(socket.getOutputStream());
+
+		
+    	initialOutput(outputFileTcp);
+    	List<String[]> operations = csv.readCVS(inputFile);
+    	for (int i = 1; i <operations.size(); i++) {
+    		String operate = operations.get(i)[0];
+    		String key = operations.get(i)[1];
+    		String value = "";
+    		if (operate.equals("PUT"))
+    			value = operations.get(i)[2];
+    		
+    		// execute TCP
+    		String json = createJsonPayload(operate, key, value);
+    		Long requestTime = System.currentTimeMillis();
+    		//System.out.println(json);
+    		
+    		// protocol
+    		toServer.writeInt(0);
+    		toServer.writeInt(0);
+    		toServer.writeInt(16 + json.length());
+    		toServer.writeInt(0);
+    		toServer.writeBytes(json);
+    		toServer.flush();
+    		
+    		byte[] header = new byte[16];
+    		fromServer.read(header, 0, 16);
+    		
+    		// where magic happens
+    		int len = (int) ((((header[8] & 0xff) << 24) | ((header[9] & 0xff) << 16) | ((header[10] & 0xff) << 8) | ((header[11] & 0xff) << 0)));    
+    		byte[] message = new byte[len];
+    		fromServer.read(message, 0, len);
+    		System.out.println(new String(message));
+
+    		Long responseTime = System.currentTimeMillis();
+    		
+    		String duration = String.valueOf(responseTime - requestTime);
+    		String[] record = {operate, key, value, String.valueOf(message), duration};
+    		csv.appendCSV(outputFileTcp, record);
+    	}
+    	toServer.close();
+    	fromServer.close();
+    	socket.close();   	
+    }
+    
+    
     
     // Initialize output file
     public static void initialOutput(String outputFile) {
