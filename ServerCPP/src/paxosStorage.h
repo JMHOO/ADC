3c40c4113542d5405c4e48c9d23294f21b40268a
binzhang22 <binzhang@uw.edu>
2016-11-19 22:50:29 -0800

    add storage for acceptor status

--- ServerCPP/src/paxosStorage.h
+++ ServerCPP/src/paxosStorage.h
@@ -23,18 +23,20 @@ namespace Paxos
         void Init();	//Init() check if default storage file is exist. Create file if not exist 
     	std::string Seek(const uint64_t instanceId);
     	void write(const uint64_t instanceId, const uint64_t proposalId, const std::string value);
-    	std::string getFilePath();
+        std::string getStorageFilePath();
         uint64_t GetMaxInstanceID();
         
+        std::string getAccepterFilePath();
         bool WriteState(const uint64_t promisedProposalID, const int promisedNodeID, const uint64_t acceptedProposalID, const int acceptedNodeID, const std::string value);
         bool ReadState(uint64_t promisedProposalID, int& promisedNodeID, uint64_t& acceptedProposalID, int& acceptedNodeID, std::string& value);
         
     private:
         ILog* logger;
         
-        const std::string file_path = "paxosStorage.txt";	//default storage file
-        void CreateFile();
-        bool IsFileExist();
+        const std::string storage_file = "paxosStorage.txt";
+        const std::string accepter_file = "paxosAccepter.txt";
+        void CreateFile(std::string file);
+        bool IsFileExist(std::string file);
     };
 }
 
